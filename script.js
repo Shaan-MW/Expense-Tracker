8cee778c1ba396c26cfcf2bf3d1270a559d9e2c5
Shaan-MW <shaanmadhiwanan72@gmail.com>
2025-07-07 08:26:36 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const balance = document.querySelector('.balance');
+const incomeAmount = documet.querySelector('.income-amount');
+const expenseAmount = document.querySelector('expense-amount');
+const transactionsList = document.querySelector('.transactions');
+const descriptionEl = document.getElementById('#description');
+const amountEl = document.getElementById('#amount');
+const transactionFormEl = document.querySelector('forms');
