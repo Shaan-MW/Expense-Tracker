c7989e0a84ba14fdcc2c45c1897318d828e5b74a
Shaan-MW <shaanmadhiwanan72@gmail.com>
2025-07-25 11:27:18 +0530

    Forms
    Update of Forms Element

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const balance = document.querySelector('.balance');
-const incomeAmount = documet.querySelector('.income-amount');
-const expenseAmount = document.querySelector('expense-amount');
+const incomeAmount = document.querySelector('.income-amount');
+const expenseAmount = document.querySelector('.expense-amount');
 const transactionsList = document.querySelector('.transactions');
 const descriptionEl = document.getElementById('#description');
 const amountEl = document.getElementById('#amount');
-const transactionFormEl = document.querySelector('forms');
+const transactionFormEl = document.querySelector('.forms');
+
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+transactionFormEl.addEventListener('submit', addTransaction);
+function addTransaction(e){
+    e.preventDefault();
+
+    const description = descriptionEl.value.trim();
+    const amount = parseFloat(amountEl.value);
+
+    console.log(description);
+    console.log(typeof amount);
+}
